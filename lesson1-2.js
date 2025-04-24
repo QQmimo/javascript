33261544b7decf812aa98b2d67889ec3a8f0abd2
Чистяков Дмитрий <62027388+QQmimo@users.noreply.github.com>
2025-04-24 16:54:49 +0500

    Update lesson1-2.js

--- lesson1-2.js
+++ lesson1-2.js
@@ -12,9 +12,11 @@ console.log(numberThree);
 
 // stage 4
 let numberFour_One = Math.floor(0.3);
-let numberFour_Two = Math.round(0.6);
+let numberFour_Two = Math.ceil(0.6);
+let numberFour_Three = Math.round(0.6);
 console.log(numberFour_One);
 console.log(numberFour_Two);
+console.log(numberFour_Three);
 
 // stage 5
 console.log(NaN == NaN);
