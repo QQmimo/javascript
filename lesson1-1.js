3afa8c59b49d622fb30cc55d4951c1c48b5a0527
Чистяков Дмитрий <62027388+QQmimo@users.noreply.github.com>
2025-04-24 16:20:22 +0500

    Create lesson1-1.js

---
+++ lesson1-1.js
@@ -0,0 +1,14 @@
+const NAME = 'name';        // Объявл константу NAME
+let Surname = 'surname';    // Объявил переменную Surname
+
+console.log(NAME);          // Вывел в консоль значение константы NAME
+console.log(Surname);       // Вывел в консоль значение переменной Surname
+
+console.log(NAME, Surname); // Вывел в консоль значение константы NAME и переменной Surname
+
+surname = 'new surname';    // Изменил значение переменной Surname
+
+console.log(NAME);          // Вывел в консоль значение константы NAME
+console.log(Surname);       // Вывел в консоль значение переменной Surname
+
+console.log(NAME, Surname); // Вывел в консоль значение константы NAME и переменной Surname
