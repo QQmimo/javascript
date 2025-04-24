a67cdf0064dea38b2e939b0295550cc3298240fc
Чистяков Дмитрий <62027388+QQmimo@users.noreply.github.com>
2025-04-24 16:51:10 +0500

    Update lesson1-2.js

--- lesson1-2.js
+++ lesson1-2.js
@@ -1,11 +1,20 @@
+// stage 1
 let numberOne = 1e10;
 console.log(numberOne);
+
+// stage 2
 let numberTwo = 1e-5;
 console.log(numberTwo);
+
+// stage 3
 let numberThree = Number('1000');
 console.log(numberThree);
+
+// stage 4
 let numberFour_One = Math.floor(0.3);
 let numberFour_Two = Math.round(0.6);
 console.log(numberFour_One);
 console.log(numberFour_Two);
+
+// stage 5
 console.log(NaN == NaN);
