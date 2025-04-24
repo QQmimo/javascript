5b658f9a292e0e31de53275f9b769c59e0be854b
Чистяков Дмитрий <62027388+QQmimo@users.noreply.github.com>
2025-04-24 16:23:00 +0500

    Update lesson1-1.js

--- lesson1-1.js
+++ lesson1-1.js
@@ -4,11 +4,9 @@ let Surname = 'surname';    // Объявил переменную Surname
 console.log(NAME);          // Вывел в консоль значение константы NAME
 console.log(Surname);       // Вывел в консоль значение переменной Surname
 
-console.log(NAME, Surname); // Вывел в консоль значение константы NAME и переменной Surname
-
-surname = 'new surname';    // Изменил значение переменной Surname
-
-console.log(NAME);          // Вывел в консоль значение константы NAME
-console.log(Surname);       // Вывел в консоль значение переменной Surname
+/*
+ Изменил значение переменной Surname
+*/
+surname = 'new surname';    
 
 console.log(NAME, Surname); // Вывел в консоль значение константы NAME и переменной Surname
